3dfd5dcf2e1809d6dea46d3b2c2462d913289221
djmoberg <daniel@moberg.no>
2018-06-09 11:46:17 +0200

    env2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello Me " + process.env.sites_test_app_dan_name.defaultValue);
+    response.end("Hello Me " + process.env.Test_Pass);
 
 });
 
